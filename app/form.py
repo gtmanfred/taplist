3dbae2ab1de89c97241fe879dabf99bd817711bd
Daniel Wallace <danielwallace@gtmanfred.com>
2014-07-13 14:04:52 -0500

    reset form

--- app/form.py
+++ app/form.py
@@ -1,6 +1,7 @@
 from flask.ext.wtf import Form
 from wtforms import TextField
 from wtforms.validators import Required
+from werkzeug.datastructures import MultiDict
 
 class BeerForm(Form):
     beername = TextField('beername', validators = [Required()])
@@ -10,3 +11,7 @@ class BeerForm(Form):
     pricepint = TextField('pint price', validators = [Required()])
     pricehalf = TextField('half growler price')
     pricegrowler = TextField('growler price')
+
+    def reset(self):
+        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+        self.process(blankData)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from app.form import BeerForm
 import redis
@@ -7,28 +7,31 @@ import json
 @app.route('/entry', methods = ['POST'])
 def entry():
     form = BeerForm()
-    if hasattr(form.beername, 'data') and form.beername.data:
-        pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
-        r = redis.Redis(connection_pool=pool)
-        beer = {
-            'name': form.beername.data,
-            'brewery': form.brewery.data,
-            'type': form.beertype.data,
-            'content': form.alcohols.data,
-            'pint': int(form.pricepint.data),
-        }
-        if form.pricehalf.data:
-            beer['half'] = int(form.pricehalf.data),
-        else:
-            beer['half'] = beer['pint'] + 2
+    if request.method == 'POST':
+        if form.validate()
+            pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+            r = redis.Redis(connection_pool=pool)
+            beer = {
+                'name': form.beername.data,
+                'brewery': form.brewery.data,
+                'type': form.beertype.data,
+                'content': form.alcohols.data,
+                'pint': int(form.pricepint.data),
+            }
+            if form.pricehalf.data:
+                beer['half'] = int(form.pricehalf.data),
+            else:
+                beer['half'] = beer['pint'] + 2
 
-        if form.pricegrowler.data:
-            beer['growler'] = int(form.pricegrowler.data)
-        else:
-            beer['growler'] = beer['half'] * 2
+            if form.pricegrowler.data:
+                beer['growler'] = int(form.pricegrowler.data)
+            else:
+                beer['growler'] = beer['half'] * 2
 
-        r.set('beer_{0}'.format(form.beername.data.replace(' ', '')), json.dumps(beer))
-        r.save()
+            r.set('beer_{0}'.format(form.beername.data.replace(' ', '')), json.dumps(beer))
+            r.save()
+            form.reset()
+
     return render_template('entry.html', title = 'Entry', form = form)
 
 @app.route('/entry', methods = ['GET'])
