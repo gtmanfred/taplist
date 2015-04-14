fb1b286794de39474370b6d233dc241531d918ac
Daniel Wallace <danielwallace@gtmanfred.com>
2015-04-14 16:42:46 -0500

    can only have one reference

--- taplist/__init__.py
+++ taplist/__init__.py
@@ -22,7 +22,6 @@ app.add_url_rule('/<location>/entry', view_func=views.Entry.as_view('entry'), me
 app.add_url_rule('/<location>/scroll', view_func=views.Scroll.as_view('scroll'), methods=['GET'])
 app.add_url_rule('/<location>/json', view_func=views.Json.as_view('get_json'), methods=['GET'])
 app.add_url_rule('/<location>/edit', view_func=views.Edit.as_view('editlist'), methods=['GET', 'POST'])
-app.add_url_rule('/<location>', view_func=views.BarLists.as_view('bars'))
 app.add_url_rule('/<location>/', view_func=views.BarLists.as_view('bars'))
 app.add_url_rule('/login', view_func=views.Login.as_view('login'), methods=['GET', 'POST'])
 app.add_url_rule('/logout', view_func=views.Logout.as_view('logout'))
