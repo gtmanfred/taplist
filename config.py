a0d51832226a4cc96f2a978164d9e3961cae3148
Daniel Wallace <danielwallace@gtmanfred.com>
2015-05-03 15:05:33 -0500

    add stormpath

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os.path
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = 'someprivatestringhere'
+STORMPATH_API_KEY_FILE = os.path.expanduser('~/.apiKey.properties')
+STORMPATH_APPLICATION = 'taplists.beer'
