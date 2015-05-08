16c7ff28a9608459f9098e240d6859dfbdcb3fa3
Daniel Wallace <danielwallace@gtmanfred.com>
2015-05-08 00:34:10 -0500

    add entry page stuff for taplist

--- taplist-client/main.js
+++ taplist-client/main.js
@@ -6,8 +6,18 @@ taplist.config(function($locationProvider) {
   });
 });
 taplist.controller('TaplistController', ['$scope', '$log', '$http', '$location', function($scope, $log, $http, $location) {
-  $log.log($location.search()['name']);
-  $scope.beerdata = {};
+  $scope.beerdata = {
+    beerid: "",
+    beername: "",
+    brewery: "",
+    beertype: "",
+    alcohols: "",
+    pricepint: "",
+    pricehalf: "",
+    pricegrowler: "",
+    notes: "",
+    active: ""
+  };
   function get_locations(){
     $http.get('https://taplists.beer/locations').
       success(function(data, status, headers, config) {
@@ -42,6 +52,25 @@ taplist.controller('TaplistController', ['$scope', '$log', '$http', '$location',
   if ($scope.loc.indexOf('/entry') > -1) {
       $scope.entry = true;
       $scope.loc = $scope.loc.replace('/entry', '');
+      var beerid = $location.search()['name'];
+      if (beerid) {
+        var url = 'https://taplists.beer' + $scope.loc + '/' + beerid;
+        $http.get(url).
+          success(function(data, status, headers, config) {
+            $scope.beerdata.beerid = beerid;
+            $scope.beerdata.beername = data['beer']['name'];
+            $scope.beerdata.brewery = data['beer']['brewery'];
+            $scope.beerdata.beertype = data['beer']['type'];
+            $scope.beerdata.alcohols = data['beer']['content'];
+            $scope.beerdata.pricepint = data['beer']['pint'];
+            $scope.beerdata.pricehalf = data['beer']['half'];
+            $scope.beerdata.pricegrowler = data['beer']['growler'];
+            $scope.beerdata.notes = data['beer']['notes'];
+            $scope.beerdata.active = data['beer']['active'];
+          }).error(function(error){
+            $log.log(error);
+          });
+      };
   } else {
       $scope.entry = false;
   };
@@ -50,4 +79,33 @@ taplist.controller('TaplistController', ['$scope', '$log', '$http', '$location',
       get_beers();
   };
   get_locations();
+  $scope.entry = function(){
+    var payload = {
+        "brewery": $scope.beerdata.brewery,
+        "beername": $scope.beerdata.beername,
+        "beertype": $scope.beerdata.beertype,
+        "alcohols": $scope.beerdata.alcohols,
+        "active": $scope.beerdata.active,
+        "pricepint": $scope.beerdata.pricepint,
+        "pricehalf": $scope.beerdata.pricehalf,
+        "pricegrowler": $scope.beerdata.pricegrowler,
+        "notes": $scope.beerdata.notes
+    };
+    if ($scope.beerid) {
+      payload.name = $scope.beerdata.beerid;
+      $http.put("https://taplists.beer/entry", payload).
+        success(function(data, status, headers, config) {
+          $log.log(status);
+        }).error(function(error){
+          $log.log(error);
+        });
+    } else {
+      $http.post("https://taplists.beer/entry", payload).
+        success(function(data, status, headers, config) {
+          $log.log(status);
+        }).error(function(error){
+          $log.log(error);
+        });
+    };
+  };
 }]);
